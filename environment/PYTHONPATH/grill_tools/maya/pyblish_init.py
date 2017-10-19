da8a4abb321147e757c89f8400e03f82731ad1b7
Toke Jepsen <tokejepsen@gmail.com>
2017-10-19 17:31:13 +0200

    instance toggling logic are determined by the instance.

--- environment/PYTHONPATH/grill_tools/maya/pyblish_init.py
+++ environment/PYTHONPATH/grill_tools/maya/pyblish_init.py
@@ -2,37 +2,14 @@ import pyblish.api
 from grill_tools import pyblish_utils
 
 
-# Pyblish callbacks for presisting instance states to the scene.
-def toggle_instance(instance, new_value, old_value):
+# Pyblish callbacks for presisting instance states to the scene
+def instance_toggled(instance, new_value, old_value):
 
-    node = instance[0]
-
-    families = instance.data.get("families", [])
-    if "cache" in families or "scene" in families:
-        attrs = []
-        for attr in node.listAttr(userDefined=True):
-            attrs.append(attr.name(includeNode=False))
-
-        attr_list = list(set(attrs) & set(families))
-
-        if attr_list:
-            node.attr(attr_list[0]).set(new_value)
-
-    if "renderlayer" in instance.data.get("families", []):
-
-        node.renderable.set(new_value)
-
-    if "playblast" in instance.data.get("families", []):
-
-        node.getTransform().publish.set(new_value)
-
-    if "file" in instance.data.get("families", []):
-
-        node.publish.set(new_value)
+    instance.data["instanceToggled"](instance, new_value)
 
 
 def init():
 
     pyblish_utils.init()
 
-    pyblish.api.register_callback("instanceToggled", toggle_instance)
+    pyblish.api.register_callback("instanceToggled", instance_toggled)
